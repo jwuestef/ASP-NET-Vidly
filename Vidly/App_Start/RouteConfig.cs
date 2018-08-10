c66a50503e4f44dc1d3b6693a8c32fa5177bcafd
jwuestef <jwuestef@gmail.com>
2018-08-10 09:46:11 -0400

    end of video #18, near the end of section 2

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -13,6 +13,19 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            // Use regular expressions to specify that the year must be either 2015 or 2016 and the month must be 2 digits
+            // OLD WAY DONT USE THIS
+            //routes.MapRoute(
+            //    "MoviesByReleaseDate",
+            //    "movies/released/{year}/{month}",
+            //    new { controller = "Movies", action = "ByReleaseDate" },
+            //    new { year = @"2015|2016", month = @"\d{2}" }
+            //);
+
+            // INSTEAD USE THIS
+            // Use this line, and then use [Route] attributes in the controllers themselves
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ViewResult Random()   // Switched from ActionResult to ViewResult
+        {
+            var movie = new Movie() { Name = "Shrek" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1"},
+                new Customer { Name = "Customer 2"}
+            };
+
+            //ViewData["Movie"] = movie;  // BAD because of string, not easily updated
+            //ViewBag.Movie = movie; // ALSO bad...
+            //return View();
+
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+
+            // JUST PASS IT TO THE VIEW THIS WAY:
+            //return View(movie);
+            return View(viewModel);
+
+            //return Content("Hello World!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+
+
+        public ActionResult Edit(int id)   // called 'id' in the route config
+        {
+            return Content("id=" + id);
+        }
+
+
+
+        // when /movies is hit
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+
+
+        [Route("movies/released/{year:regex(\\d{4})}/{month:range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+
+
+    }
+}
